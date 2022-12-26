b3fcd76ea06d922fb728345d52646647855a33c1
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2022-12-26 12:21:35 +0100

    AppController - DownloadController - DownloadTask -> Funcionando arranca el Gestor de Descargas

--- src/main/java/com/svalero/gesdescarga/controller/AppController.java
+++ src/main/java/com/svalero/gesdescarga/controller/AppController.java
@@ -24,14 +24,14 @@ public class AppController {
     public Button btDownload; // Botón que usamos para la descarga en JavaFx
     public TabPane tpDownloads; //Panel creado en JavaFx para que las descargas se añadan en pestañas
 
-//    private Map<String, DownloadController> allDownloads; // Creamos un mapa para guardar todas las descargas
+    private Map<String, DownloadController> allDownloads; // Creamos un mapa para guardar todas las descargas
 
     /**
      * Método para inicializar el Mapa de las descargas de DownloadController
      */
-//    public AppController() {
-//        allDownloads = new HashMap();
-//    }
+    public AppController() {
+        allDownloads = new HashMap();
+    }
 
     /**
      * Método que se ejecuta cuando pulsamos el botón descarga, lo ponemos en onAction  Scene Builder para que JavaFx lo encuentre
@@ -41,36 +41,103 @@ public class AppController {
      */
     @FXML
     public void launchDownload(ActionEvent event) {
+
         String urlText = tfUrl.getText(); //Metemos el contenido de la caja de texto en un String
         tfUrl.clear(); //Para vaciar la caja de texto una vez recogido el texto y metido en un String
         tfUrl.requestFocus(); //Volvemos a pillar el foco para poder meter otra Url rápidamente
 
-//        launchDownload(urlText); //Llama a otro método que se llama igual pero con un parámetro String para la Url. Sobrecargar el método.
+        launchDownload(urlText); //Llama a otro método que se llama igual pero con un parámetro String para la Url. Sobrecargar el método.
     }
 
     /**
      * Sobrecargamos el método anterior y le pasamos un String
      * @param url
      */
-//    private void launchDownload(String url) {
-//
-//        try {
-//            FXMLLoader loader = new FXMLLoader(); //Creamos un objeto FMXLloader que se encargará de Montarnos la interfaz de lo otra ventana
-//            //Todo cambiar por la ventana que necesitamos presentar al realizar las descargas
-//            loader.setLocation(R.getUI("gesDescargaHome.fxml")); // Le pasamos la localización de la ventana diseñada con JavaFx
-//
-//            DownloadController downloadController = new DownloadController(url); //Creamos su propio controler desde su clase DownloadController para gestionar los botones y demás cosas
-//            loader.setController(downloadController);
-//            //Todo revisar si crearé un VBox o el padre será de otro tipo
-//            VBox downloadBox = loader.load(); //En este caso el padre de la ventana es un Vbox en JavaFx
-//
-//            String filename = url.substring(url.lastIndexOf("/") + 1); //para añadirle a cada pestaña el final de la URL que le pasamos, gracias al filename.
-//            tpDownloads.getTabs().add(new Tab(filename, downloadBox)); //Lo añadimos al panel de ventana de "multidownload.fxml"y añadimos una PESTAÑA por cada descarga que le damos.
-//
-//            allDownloads.put(url, downloadController); //Cada vez que lancemos una descarga lo añadimos al mapa para poder cancelarlas todas, tenemos cada url de descarga asociado al objeto que se crea por cada descarga
-//
-//        } catch (IOException ioe) {
-//            ioe.printStackTrace(); //Pintamos la traza
-//        }
-//    }
+    private void launchDownload(String url) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(); //Creamos un objeto FMXLloader que se encargará de Montarnos la interfaz de lo otra ventana
+            //Todo cambiar por la ventana que necesitamos presentar al realizar las descargas
+            loader.setLocation(R.getUI("gesDescargaHome.fxml")); // Le pasamos la localización de la ventana diseñada con JavaFx
+
+            DownloadController downloadController = new DownloadController(url); //Creamos su propio controler desde su clase DownloadController para gestionar los botones y demás cosas
+            loader.setController(downloadController);
+            //Todo revisar si crearé un VBox o el padre será de otro tipo
+            VBox downloadBox = loader.load(); //En este caso el padre de la ventana es un Vbox en JavaFx
+
+            String filename = url.substring(url.lastIndexOf("/") + 1); //para añadirle a cada pestaña el final de la URL que le pasamos, gracias al filename.
+            tpDownloads.getTabs().add(new Tab(filename, downloadBox)); //Lo añadimos al panel de ventana de "multidownload.fxml"y añadimos una PESTAÑA por cada descarga que le damos.
+
+            allDownloads.put(url, downloadController); //Cada vez que lancemos una descarga lo añadimos al mapa para poder cancelarlas todas, tenemos cada url de descarga asociado al objeto que se crea por cada descarga
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace(); //Pintamos la traza
+        }
+    }
+
+    /**
+     * Quitar comentario para descarga desde fichero.
+     * @param event
+     */
+    @FXML
+    public void launchFileDownload(ActionEvent event) {
+        String urlText = tfUrl.getText();
+        tfUrl.clear();
+        tfUrl.requestFocus();
+        readDLC(); //Llamamos al método que leerá el fichero
+    }
+
+    /**
+     * Recorremos el ArrayList y cancelamos todas las descargas
+     */
+    @FXML
+    public void stopAllDownloads() {
+        for ( DownloadController downloadController : allDownloads.values()) // devuelve una coleccion de los valores
+            downloadController.stop();
+    }
+
+    /**
+     *Para leer un fichero con url de descargas
+     */
+    @FXML
+    public void readDLC() {
+        // Pedir el fichero que queremos procesar su contenido para añadir multiples descargas (FileChooser)
+        //Para preguntarnos el ficehro que queremos abrir
+        //FileChooser fileChooser = new FileChooser(); // FileChooser: Tipica ventana para buscar un fichero por windows
+        //File file = fileChooser.showSaveDialog(tfUrl.getScene().getWindow()); // Aparece con un botón que pone Save
+        //File file = fileChooser.showOpenDialog(tfUrl.getScene().getWindow()); // Aparece con un botón que pone Open
+        //if (file == null)
+        //    return;
+        // FIN Para preguntarnos donde queremos guardar el fichero
+
+        // Quitar comentario para descarga desde fichero otra opcion.
+
+        try {
+            //File dlcFile = new File("dlc.txt"); //recogia el fichero desde la raiz del proyecto, no le damos la opcion de elegir donde esta el fichero al usuario
+            //Para preguntarnos donde esta el fichero que queremosusar para leerlo y descargar su contenido
+            FileChooser fileChooser = new FileChooser(); // FileChooser: Tipica ventana para buscar un fichero por windows
+            File dlcFile = fileChooser.showOpenDialog(tfUrl.getScene().getWindow()); // Aparece una ventana para buscar el archivo que queremos leer
+            if (dlcFile == null)
+                return;
+            // FIN Para preguntarnos donde esta el fichero que queremosusar para leerlo y descargar su contenido
+
+            Scanner reader = new Scanner(dlcFile);
+            while (reader.hasNextLine()) { //leemos las lineas del fichero
+                String data = reader.nextLine();
+                System.out.println(data);
+                launchDownload(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Se ha producido un error");
+            fnfe.printStackTrace();
+        }
+
+
+        //Leo el fichero y cargo cada linea un List (Clase Files)
+
+        //Files.read //Clase gana que lee todas las lineas de uin fichero y nos devuelve un lista de String con las lineas del fichero
+
+        // Para cada linea, llamar al método launchDownload que por cada linea de fichero que le pasamos creo una pestaña de descarga
+    }
 }
---
+++ src/main/java/com/svalero/gesdescarga/task/DownloadTask.java
@@ -0,0 +1,106 @@
+package com.svalero.gesdescarga.task;
+
+import com.svalero.gesdescarga.controller.DownloadController;
+import javafx.concurrent.Task;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Extendemos de la clase Task que es la que nos permite realizar procesos de manera concurrente
+ */
+public class DownloadTask extends Task<Integer> {
+
+    private URL url;
+    private File file;
+
+    private static final Logger logger = LogManager.getLogger(DownloadController.class); //Para disponer de un log de las Descargas
+
+    public DownloadTask(String urlText, File file) throws MalformedURLException {
+        this.url = new URL(urlText);
+        this.file = file;
+    }
+
+    @Override
+    protected Integer call() throws Exception {
+        logger.trace("Descarga" + url.toString() + "iniciada");
+        updateMessage("Conectando con el servidor ..");
+
+        /**
+         * Comienzo código para descargar ficheros de internet
+         * Código para conectarnos a una URL de Internet
+         */
+        URLConnection urlConnection = url.openConnection();
+        double fileSize = urlConnection.getContentLength(); // Podemos leer el tamaño del fichero alojado en la URL
+        //Para poder poner una limitación máxima de descarga por ejemplo si no es cuenta premiun
+//        double megaSize = fileSize / 1048576; // Tamaño del fichero que queremos descargar pasado a MB
+//        if (megaSize < 10) { //Si el tamaño de la descarga es superior a 10 Mb
+//            logger.trace("Maximo tamaño de fichero alcanzado"); //Lo escribimos en el log
+//            throw new Exception("Max. size"); //Creamos una excepción para que no deje proceder a la descarga se la mandamos al controlador DownloadCrontroller
+//        }
+
+        BufferedInputStream in = new BufferedInputStream(url.openStream()); // Leemos y copiamos al disco
+        FileOutputStream fileOutputStream = new FileOutputStream(file);// Objeto java que permite escribir en un disco
+        byte dataBuffer[] = new byte[1024];
+        int bytesRead;
+        int totalRead = 0;
+        double downloadProgress = 0;
+        //Dos formas de medir el tiempo de descarga Libreria de JAva estandar o Clase de apache commos
+        //Instant es de la libreia básica de JAVA la comentamos para usar las de apache commons
+//        Instant start = Instant.now(); //Libreria que permite calcular el tiempo que lleva la descarga recogemos el método now
+//        Instant current; //Tiempo que lleva descargando
+//        float elapsedTime; //Tiempo calculado
+
+        //Clase StopWatch de apache commons para medir el tiempo de la descarga
+        StopWatch watch = new StopWatch(); //Creamos el objeto
+        watch.start(); //Inicializamos el cronometro ya no guardamos el tiempo, el objeto se encarga de hacerlo a diferencia de si usamos la libreria de java
+
+        /**
+         * Con un buble ir leyendo trocito a trocito de la descarga
+         * Calcular la velocidad, el avanza, el progreso de la descarga.
+         * Código listo para usar para realizar descargas de Internet
+         */
+        while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+            downloadProgress = ((double) totalRead / fileSize);
+
+            updateProgress(downloadProgress, 1);
+            //updateMessage(downloadProgress * 100 + " % "); Escribir en la barra de progreso lo comento porque usare o la opcion JAva o Apache commons
+
+            //PAra la libreria java
+//            current = Instant.now(); //Calcular el tiempo actual por cada kilobytes que vamos descargando en cada interaccion
+//            elapsedTime = Duration.between(start, current).toSeconds();
+//            updateMessage(Math.round(downloadProgress *100) + "%\t\t\t\t" +Math.round(elapsedTime) + "sec."); //Para redondear el porcentaje de descarga completado. \t -> para separar, son tabuladores
+
+            //Para usar la libreria de Apache Commons gracias a watch.getTime se encarga de ir realizando el calculo de tiempo
+            updateMessage(Math.round(downloadProgress * 100) + " %\t\t\t\t" + Math.round(watch.getTime()/1000) + " sec."); //Para redondear el porcentaje de descarga completado. \t -> para separar, son tabuladores
+
+            //Modificado para hacer la descarga más lenta
+            //Thread.sleep(1); //Cada bloque de descarga de 1024 Mb para el Thread un milisegundo
+
+            fileOutputStream.write(dataBuffer, 0, bytesRead); //Escribir el disco HD
+            totalRead += bytesRead;
+            /**
+             * Fin Código para realizar descargas de internet
+             */
+
+            if (isCancelled()) {
+                logger.trace("Descarga " + url.toString() + " cancelada");
+                return null;
+            }
+        }
+
+        updateProgress(1, 1); //Para actualizar la barra de proceso
+        updateMessage("100 %"); //Mensaje cuando termina
+
+        logger.trace("Descarga " + url.toString() + " finalizada");
+
+        return null;
+    }
+}
